647bc72f29862b8645b998abec7cec9f4ff37f18
Kolby Skolrood <kolbyjskolrood@gmail.com>
2026-06-18 13:03:48 -0700

    fix: revert to psycopg2-binary, disable uv in favor of pip

--- api/_db.py
+++ api/_db.py
@@ -1,34 +1,41 @@
-import psycopg
-from psycopg.rows import dict_row
+import psycopg2
+import psycopg2.extras
 from contextlib import contextmanager
 from _config import DATABASE_URL
 
 
 @contextmanager
 def get_db():
-    with psycopg.connect(DATABASE_URL, row_factory=dict_row) as conn:
+    conn = psycopg2.connect(DATABASE_URL)
+    try:
         yield conn
+        conn.commit()
+    except Exception:
+        conn.rollback()
+        raise
+    finally:
+        conn.close()
 
 
 def fetchone(sql, params=None):
     with get_db() as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
             cur.execute(sql, params)
             return cur.fetchone()
 
 
 def fetchall(sql, params=None):
     with get_db() as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
             cur.execute(sql, params)
             return cur.fetchall()
 
 
 def execute(sql, params=None):
     with get_db() as conn:
-        with conn.cursor() as cur:
+        with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
             cur.execute(sql, params)
             try:
                 return cur.fetchone()
-            except Exception:
+            except psycopg2.ProgrammingError:
                 return None
