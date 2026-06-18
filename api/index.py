3c23115650aaf694a3892369d3bbcaa591d9c8b3
Kolby Skolrood <kolbyjskolrood@gmail.com>
2026-06-18 13:04:02 -0700

    fix: remove python-version file to let Vercel use pip instead of uv

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Make sibling modules (prefixed _) importable when run by Vercel
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from flask import Flask, jsonify
@@ -23,4 +22,4 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
